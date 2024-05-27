f65a72f3a19d77b7c6367c5798e9e22d1b56c324
Ayu <claire@ayu.land>
2024-05-27 12:55:24 +0800

    🚧 Correctly running the algorithm

--- Distributor/misc/camera_vis/main.c
+++ Distributor/misc/camera_vis/main.c
@@ -1,6 +1,8 @@
 // gcc main.c -Ilibs/minifb/include -Ilibs/openpnp-capture/include build_libs/minifb/libminifb.a build_libs/openpnp-capture/libopenpnp-capture.0.dylib build_libs/stb_image_resize2.o -framework AppKit -framework Metal -framework MetalKit
 // DYLD_LIBRARY_PATH=build_libs/openpnp-capture ./a.out
 
+#include "silhouette_detection.h"
+
 #include "MiniFB.h"
 #include "openpnp-capture.h"
 #include "stb_image_resize2.h"  // stb_image_resize2 - v2.06 (2fb057a)
@@ -8,12 +10,20 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 static void cb_keyboard(struct mfb_window *window, mfb_key key, mfb_key_mod mod, bool is_pressed)
 {
   if (is_pressed &&
       (key == KB_KEY_ESCAPE || (key == KB_KEY_Q && (mod & KB_MOD_SUPER))))
     exit(0);
+}
+
+static uint64_t millitime()
+{
+  struct timespec t;
+  clock_gettime(CLOCK_REALTIME, &t);
+  return t.tv_sec + t.tv_nsec / 1000000;
 }
 
 int main()
@@ -23,7 +33,7 @@ int main()
   struct mfb_window *window = mfb_open_ex("camera test", 320, 240, WF_RESIZABLE);
   mfb_set_keyboard_callback(window, cb_keyboard);
 
-  uint8_t *p = malloc(160 * 120 * 4);
+  uint8_t *p = malloc(640 * 480 * 4);
 
   // ============ OpenPnP Capture initialisation ============
 
@@ -55,7 +65,14 @@ int main()
 
   // openpnp-capture.h: "the frames returned by Cap_captureFrame are always 24-bit RGB."
   uint8_t *p_raw = malloc(fmt_info.width * fmt_info.height * 3);
-  uint8_t *p_scaled = malloc(160 * 120 * 3);
+  uint8_t *p_scaled = malloc(640 * 480 * 3);
+
+  uint8_t *p_ycbcr = malloc(640 * 2);
+
+  static struct silhouette_detection d;
+  silhouette_init(&d);
+
+  uint64_t t0 = millitime();
 
   do {
     if (Cap_hasNewFrame(ctx, stream) == 1) {
@@ -64,19 +81,39 @@ int main()
 
     stbir_resize_uint8_srgb(
       p_raw, fmt_info.width, fmt_info.height, fmt_info.width * 3,
-      p_scaled, 160, 120, 160 * 3,
+      p_scaled, 640, 480, 640 * 3,
       STBIR_RGB);
-    for (int r = 0; r < 120; r++) {
-      for (int c = 0; c < 160; c++) {
+    for (int r = 0; r < 480; r++) {
+      for (int c = 0; c < 640; c++) {
         // BGRA
-        p[(r * 160 + c) * 4 + 0] = p_scaled[(r * 160 + c) * 3 + 2];
-        p[(r * 160 + c) * 4 + 1] = p_scaled[(r * 160 + c) * 3 + 1];
-        p[(r * 160 + c) * 4 + 2] = p_scaled[(r * 160 + c) * 3 + 0];
-        p[(r * 160 + c) * 4 + 3] = 255;
+        p[(r * 640 + c) * 4 + 0] = p_scaled[(r * 640 + c) * 3 + 2];
+        p[(r * 640 + c) * 4 + 1] = p_scaled[(r * 640 + c) * 3 + 1];
+        p[(r * 640 + c) * 4 + 2] = p_scaled[(r * 640 + c) * 3 + 0];
+        p[(r * 640 + c) * 4 + 3] = 255;
       }
     }
 
-    mfb_update_ex(window, p, 160, 120);
+    uint64_t t1 = millitime();
+    if (t1 - t0 >= 100) {
+      // puts("*");
+      t0 = t1;
+      for (int r = 0; r < 480; r++) {
+        for (int c = 0; c < 640; c++) {
+          uint8_t luma = (
+            (uint32_t)p_scaled[(r * 640 + c) * 3 + 2] * 299 +
+            (uint32_t)p_scaled[(r * 640 + c) * 3 + 1] * 587 +
+            (uint32_t)p_scaled[(r * 640 + c) * 3 + 0] * 114
+          ) / 1000;
+          // Assumes little-endian
+          p_ycbcr[(c ^ 1) * 2 + 0] = 0;
+          p_ycbcr[(c ^ 1) * 2 + 1] = luma;
+        }
+        silhouette_feed_line(&d, (uint32_t *)p_ycbcr);
+      }
+      silhouette_end_frame(&d);
+    }
+
+    mfb_update_ex(window, p, 640, 480);
   } while (mfb_wait_sync(window));
 
   free(p);
